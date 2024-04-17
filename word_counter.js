4499131b590e0f375be47835592ac4c5e9e3270a
DELL <afridi2542@gmail.com>
2024-04-17 16:49:13 +0500

    first commit

---
+++ word_counter.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.magentaBright("***Welcome to the word counter ***"));
+let userAnswer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "sentence",
+        message: chalk.bold.magenta("Enter your sentence to count the words."),
+    }
+]);
+let words = userAnswer.sentence.trim().split(" ");
+// print the array of words to the console
+console.log(words);
+// print the word count sentence to the console
+console.log(chalk.bold.greenBright(`Your sentence word count is ${words.length}`));
